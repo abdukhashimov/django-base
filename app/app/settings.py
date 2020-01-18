9afe4a70e27af1c2f7b1ea86c22a5b84f58481f4
Madiyor <abdukhashimov@yandex.ru>
2020-01-18 10:50:33 +0500

    configured postgresql, added wait_for_db

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     ######
-    #custom apps
+    # custom apps
     'core.apps.CoreConfig',
 ]
 
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gsoft',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
